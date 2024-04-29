15d3cff552e033b0f89a793a1ba2e275fbf0764a
Kevin Cabral <kevin1tdsb@gmail.com>
2024-04-29 17:34:51 -0300

    Add serializers and views tests

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for bookstore project.
+"""bookstore URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,8 +15,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
+    re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
+    path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
 ]
